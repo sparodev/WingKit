fe899c05a7f1f6f82a12585f9bb02d8277b95d04
Matt Wahlig <mwahlig17@gmail.com>
2017-09-21 12:58:57 -0500

    Initial Commit

---
+++ WingKit/WingKit.h
@@ -0,0 +1,19 @@
+//
+//  WingKit.h
+//  WingKit
+//
+//  Created by Matt Wahlig on 9/21/17.
+//  Copyright © 2017 Sparo Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WingKit.
+FOUNDATION_EXPORT double WingKitVersionNumber;
+
+//! Project version string for WingKit.
+FOUNDATION_EXPORT const unsigned char WingKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WingKit/PublicHeader.h>
+
+
